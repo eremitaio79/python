8b6baaa89e5888c6a602da0423b8563f0a5ea9ab
Paulo Eremita <eremitaio79@gmail.com>
2024-12-31 18:13:05 -0300

    Exercícios resolvidos 1b

--- exercises/desafio0012.py
+++ exercises/desafio0012.py
@@ -1,3 +1,15 @@
 """
 Faça um programa que leia o preço de um produto e mostre seu novo preço com 5% de desconto.
 """
+
+# Entrada.
+preco_produto = float(input('Entre o valor do produto em R$: '))
+
+# Calcula o desconto no preço do produto.
+valor_desconto = 0.05
+preco_com_desconto = preco_produto - (preco_produto * valor_desconto)
+
+# Output.
+print(f'Preço do produto: R$ {preco_produto:.2f}')
+print(f'Valor do desconto: {valor_desconto * 100}%')
+print(f'Preço final com desconto: R$ {preco_com_desconto:.2f}')
--- exercises/desafio0013.py
+++ exercises/desafio0013.py
@@ -1,3 +1,15 @@
 """
 Escreva um programa que leia o salário de um funcionário e mostre seu novo salário com 15% de aumento.
 """
+
+# Entrada.
+salario_bruto = float(input('Entre o valor do salário em R$: '))
+
+# Calcula o valor do reajuste salarial.
+valor_reajuste = 0.15
+salario_com_aumento = salario_bruto + (salario_bruto * valor_reajuste)
+
+# Output.
+print(f'Salário atual: R$ {salario_bruto:.2f}')
+print(f'Valor do aumento: {valor_reajuste * 100}%')
+print(f'Salário reajustado: R$ {salario_com_aumento:.2f}')
