a4186ca854a34b351bc6fc7bec302c1d23cc8e99
Paulo Eremita <eremitaio79@gmail.com>
2025-01-02 22:59:35 -0300

    Exercícios resolvidos 2

---
+++ exercises/desafio0014.py
@@ -0,0 +1,3 @@
+"""
+Escreva um programa que converta uma temperatura de graus Célsius para graus Fahrenheit.
+"""
---
+++ exercises/desafio0020.py
@@ -0,0 +1,4 @@
+"""
+O mesmo professor do desafio 19 quer sortear a ordem da apresentação de trabalhos dos alunos. Escreva
+um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+"""
