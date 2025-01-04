0990bd796ab2e358f0fab6256d59dcb183e94a2a
Paulo Eremita <eremitaio79@gmail.com>
2025-01-04 08:49:57 -0300

    Exercícios resolvidos 2a

--- exercises/desafio0016.py
+++ exercises/desafio0016.py
@@ -1,3 +1,14 @@
-"""
-Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua parte inteira.
-"""
+""" Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua parte inteira. """
+
+# import math
+from math import trunc as t
+
+# Input.
+num_real = float(input('Entre um número real qualquer: '))
+
+# Processing.
+parte_inteira = t(num_real)
+
+# Output.
+print(f'Número real entrado: {num_real}')
+print(f'Parte inteira do número real: {parte_inteira}')
--- exercises/desafio0017.py
+++ exercises/desafio0017.py
@@ -1,4 +1,19 @@
-"""
-Escreva um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo,
-calcule e mostre o comprimento da hipotenusa.
-"""
+""" Escreva um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo,
+calcule e mostre o comprimento da hipotenusa. """
+
+# Modules.
+from math import sqrt as sr, pow as p
+
+# Input.
+cateto_oposto = float(input('Entre o valor do cateto oposto: '))
+cateto_adjacente = float(input('Entre o valor do cateto adjacente: '))
+
+""" Processing: Computing the hypotenuse value. """
+hipotenusa = sr(p(cateto_oposto, 2) + (p(cateto_adjacente, 2)))
+
+# Output.
+print('-' * 50)
+print(f'Cateto oposto: {cateto_oposto}')
+print(f'Cateto adjacente: {cateto_adjacente}')
+print('-' * 50)
+print(f'O valor da hipotenusa é: {hipotenusa:.3f}')
