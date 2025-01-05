681da4d9f2b8ef1c2b1e230f42810883518153f4
Paulo Eremita <eremitaio79@gmail.com>
2025-01-05 15:06:41 -0300

    Exercícios resolvidos 4

---
+++ exercises/desafio0033.py
@@ -0,0 +1,2 @@
+""" Escreva um programa que leia 3 números e mostre qual deles é
+o maior e qual é o menor. """
---
+++ exercises/desafio0035.py
@@ -0,0 +1,2 @@
+""" Escreva um programa que leia o comprimento de 3 retas e diga ao usuário
+se elas podem ou não formar um triângulo. """
