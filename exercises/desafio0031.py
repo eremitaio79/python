443c0923237891c2f92bddcd4b3db8ab0985c060
Paulo Eremita <eremitaio79@gmail.com>
2025-01-06 21:52:27 -0300

    Exercícios resolvidos 5a

--- exercises/desafio0031.py
+++ exercises/desafio0031.py
@@ -2,14 +2,20 @@
 Calcule o preço da passagem, cobrando R$ 0,50 por km para viagens de
 até 200km e R$ 0,45 para viagens mais longas. """
 
+# Input.
 distancia_viagem = float(input('Qual a distância da viagem?: '))
+
+# Parameters.
 distancia_limite = 200
 valor_multa1 = 0.50
 valor_multa2 = 0.45
 
+# Processing and output.
 if distancia_viagem <= distancia_limite:
     print(f'Distância informada: {distancia_viagem} km.')
-    print(f'O valor da passagem é R$ {distancia_viagem * valor_multa1}.')
+    valor_final = distancia_viagem * valor_multa1
+    print(f'O valor da passagem é R$ {valor_final:.2f}.')
 else:
     print(f'Distância informada: {distancia_viagem} km.')
-    print(f'O valor da passagem é R$ {distancia_viagem * valor_multa2}.')
+    valor_final = distancia_viagem * valor_multa2
+    print(f'O valor da passagem é R$ {valor_final:.2f}.')
--- exercises/desafio0032.py
+++ exercises/desafio0032.py
@@ -1,9 +1,13 @@
 """ Escreva um programa que leia um ano qualquer e diga se ele é bissexto. """
 
+from datetime import date
+
 # Input: solicitando o ano ao usuário
-ano = int(input("Digite um ano: "))
+ano = int(input("Digite um ano (para o ano atual, digite -1): "))
 
 # Verificando se é bissexto
+if ano == -1:
+    ano = date.today().year
 if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
     print(f"O ano {ano} é bissexto.")
 else:
